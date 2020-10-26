63969655ecb4c437a36b727d6c42837801408253
toddkowalczyk <toddmkowalczyk@gmail.com>
2020-10-25 22:30:40 -0400

    fix so markers show up on map

--- client/yelpington/src/components/RestaurantList.js
+++ client/yelpington/src/components/RestaurantList.js
@@ -10,16 +10,12 @@ const mapLat = '43.6106'
 const mapLong = '-72.9726'
 const mapZoom = '13'
 
-
-
 function RestaurantList(props) {
 
     // state vars
     const [restaurantList, setRestaurantList] = useState(null)
     const [restId, setRestId] = useState(null)
-    const [latLong, setLatLong] = useState(null)
-
-    let mapPins = []
+    const [mapMarkers, setMapMarkers] = useState(null)
 
     // read list of restaurant names to display
     // then call function to setup array so markers can be placed on map
@@ -40,10 +36,10 @@ function RestaurantList(props) {
     // setup array so we can create map markers for each
     async function getLatLong(data) {
 
-        // array to hold data for map pins
-        let compArray = []
+        // array to hold lat/long data for map pins
+        let  markerArray = []
 
-        let markerCollection = []
+        // 
         let marker = {
             key: null,
             lat: null,
@@ -58,84 +54,80 @@ function RestaurantList(props) {
 
             const fetchURL = hostLocation + 'restaurant-id/' + element
 
+            // Read individual Json file and load up Marker array for each restaurant
             await fetch(fetchURL)
                 .then(res => res.json())
                 .then(restaurant => {
-                    marker.key = restaurant['name']
-                    marker.lat = restaurant['latitude']
-                    marker.long = restaurant['longitude']
-                    console.log('Marker.key is: ' + marker.key)
-                    console.log('Marker.lat is: ' + marker.lat)
-                    console.log('Marker.long is: ' + marker.long)
-
-                    compArray.push(
+                    markerArray.push(
                         <Marker
-                            key={marker.key}
+                            key={restaurant['name']}
                             position={[
-                                marker.lat,
-                                marker.long
+                                restaurant['latitude'],
+                                restaurant['longitude']
                             ]}
-                        />
-                    )
-
-
-                })
-
-               
-
-        }
-
-        console.log('compArray: ')
-        console.log(compArray)
-        setLatLong(compArray)
-        return
-    }
-
-    // If restaurant link is clicked, set restaurant ID
-    // This allows resturant details to be rendered on click of link
-    function showRestaurant(id, evt) {
-        evt.preventDefault()
-        setRestId(id)
-    }
-
-    // Render restaurant list and a link is clicked render restaurant component as well
-    // on-click handler is defined to toggle the rendering of restaurant details
-    return (
-        <div>
-            <div>
-                {restaurantList ? (
-                    restaurantList.map((id) => (
-                        <div>
-                            <a href={id} onClick={(evt) => showRestaurant(id, evt)}>{id}</a>
-                        </div>
-                    ))
-                ) : (
-                        <p>...Loading</p>
-                    )}
-                {restId &&
-                    <Restaurant url={restId}></Restaurant>}
-            </div>
-
-            <div>
-                <Map center={[mapLat, mapLong]} zoom={mapZoom} >
-
-                    <TileLayer
-                        attribution='&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            onClick={(evt) => {
+                                markerClicked(marker.key, evt);
+                              }}
                     />
+                )
 
-                    {
-                        latLong
-                    }
+        })
+    }
+    // Update State with array of Marker info
+    setMapMarkers(markerArray)
+    return
+}
 
-                </Map>
+//  If marker is clicked, show restaurant page
+function markerClicked(markerKey, evt) {
+    alert('clicked')
+}
 
+// If restaurant link is clicked, set restaurant ID
+// This allows resturant details to be rendered on click of link
+function showRestaurant(id, evt) {
+    evt.preventDefault()
+    setRestId(id)
+}
 
-
-            </div>
+// Render restaurant list as links
+// And map with restaurant markers
+return (
+    <div>
+        <div>
+            {restaurantList ? (
+                restaurantList.map((id) => (
+                    <div>
+                        <a href={id} onClick={(evt) => showRestaurant(id, evt)}>{id}</a>
+                    </div>
+                ))
+            ) : (
+                    <p>...Loading</p>
+                )}
+            {restId &&
+                <Restaurant url={restId}></Restaurant>}
         </div>
 
-    );
+        <div>
+            <Map center={[mapLat, mapLong]} zoom={mapZoom} >
+
+                <TileLayer
+                    attribution='&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+
+                {
+                    mapMarkers
+                }
+
+            </Map>
+
+
+
+        </div>
+    </div>
+
+);
 };
 
 export default RestaurantList
